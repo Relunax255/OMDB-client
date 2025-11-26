173495382b292fa237c5bf71fc0c7c9cca99f33a
KACHAN VIKTOR <KACHAN.21110>
2025-11-26 09:47:58 +0100

    implementazione parziale navigazione pagine

--- index.js
+++ index.js
@@ -6,20 +6,25 @@ window.addEventListener("load", event => {
   const TEXTINPUT_APIKEY = document.getElementById("apikey");
   const SPAN_APIKEYMESSAGE = document.getElementById("apikeymessage");
   const SPAN_SEARCHMESSAGE = document.getElementById("searchmessage");
-  BUTTON_SEARCH.addEventListener("click", event => {
-    let searchType = SELECT_SELECTTYPE.value;
-    let searchString = TEXTINPUT_SEARCH.value;
-    let apiKey = TEXTINPUT_APIKEY.value;
-    if (apiKey === "") {
-      SPAN_APIKEYMESSAGE.innerText = "inserire una chiave (api key) valida."; return;
-    } else SPAN_APIKEYMESSAGE.innerText = "";
-    if (searchString==="") {
-      SPAN_SEARCHMESSAGE.innerText = "inserire una stringa di ricerca."; return;
-    } else SPAN_SEARCHMESSAGE.innerText = "";
-    if (searchType=="")  {searchType="s"; }
-    let url = "http://www.omdbapi.com/?";
-    url+= "apikey=" + apiKey + "&";
-    url+= searchType + "=" + encodeURIComponent(searchString);
+
+  const BUTTON_PAGEXLEFT = document.getElementById("pagexleft");
+  const BUTTON_PAGELEFT = document.getElementById("pageleft");
+  const BUTTON_PAGERIGHT = document.getElementById("pageright");
+  const BUTTON_PAGEXRIGHT = document.getElementById("pagexright");
+  const SPAN_CURRENTPAGENUMBER = document.getElementById("currentpagenumber");
+  const SPAN_TOTALPAGESNUMBER = document.getElementById("totalpagesnumber");
+  let paged = false;
+  let globally_visible_searchparams = null;
+  let pages = 0;
+  const fixed_url  = "http://www.omdbapi.com/?"
+  
+  function search(searchparams) { //obj
+    DIV_SEARCHRESULTCONTAINER.innerHTML="";
+    let url = fixed_url;
+    url+= "apikey=" + searchparams.apiKey + "&";
+    url+= searchparams.searchType + "=" + encodeURIComponent(searchparams.searchString);
+    if (Number(searchparams.page)!=null&& Number(searchparams.page)>0)
+      {url+="&page="+Number(searchparams.page);}
     fetch(url)
 
       .then(response => response.json())
@@ -28,9 +33,10 @@ window.addEventListener("load", event => {
         if (data.Response==="False") {
           DIV_SEARCHRESULTCONTAINER.innerText=data.Error + "\r\nProva a modificare i criteri di ricerca"; return;
         }
-        switch (searchType) {
+        switch (searchparams.searchType) {
           case "s":
-            DIV_SEARCHRESULTCONTAINER.innerHTML="";
+           
+            if (Number(data.totalResults)>10) {paged=true; pages=Math.ceil(data.totalResults/10);}
             data.Search.forEach(itemData => {
               const item = document.createElement("div");
               item.className = "item";
@@ -42,6 +48,7 @@ window.addEventListener("load", event => {
               
               item.appendChild(img);
               let title = document.createElement("div");
+              title.style.margin="7px";
               title.innerText = itemData.Title + " (" + itemData.Year + ")";
               item.appendChild(title);
               
@@ -54,7 +61,7 @@ window.addEventListener("load", event => {
             break;
           case "i":
             //single result
-
+            paged=false;
             const item = document.createElement("div");
             item.className = "item";
             let img = document.createElement("img");
@@ -69,9 +76,56 @@ window.addEventListener("load", event => {
         }
       })
       .catch(error => {
-        alert("Errore durante la richiesta:" + error); return;
+        alert("Errore durante la richiesta: " + error); return;
       });
+  }
+
+  BUTTON_SEARCH.addEventListener("click", () => {
+    let apiKey = TEXTINPUT_APIKEY.value;
+    if (apiKey === "") {
+      SPAN_APIKEYMESSAGE.innerText = "inserire una chiave (api key) valida."; return;
+    } else SPAN_APIKEYMESSAGE.innerText = "";
+
+    let searchString = TEXTINPUT_SEARCH.value;
+    if (searchString==="") {
+      SPAN_SEARCHMESSAGE.innerText = "inserire una stringa di ricerca."; return;
+    } else SPAN_SEARCHMESSAGE.innerText = "";
+
+    let searchType = SELECT_SELECTTYPE.value;
+    if (searchType=="")  {searchType="s"; }
+    let searchparams = { searchType: searchType,
+      searchString: searchString,
+      apiKey: apiKey,
+      page: 1
+    };
+    globally_visible_searchparams = searchparams;
+    search(searchparams);
+    
   });
+
+  BUTTON_PAGEXLEFT.addEventListener("click", () => {
+    if (!paged) return;
+    let params = globally_visible_searchparams
+    params.page=1;
+    globally_visible_searchparams=params;
+    search(params);
+  });
+  BUTTON_PAGELEFT.addEventListener("click", () => {
+
+  });
+
+  BUTTON_PAGERIGHT.addEventListener("click", () => {
+    if (!paged) {return;}
+    let params=globally_visible_searchparams;
+    params.page=params.page+1;
+    globally_visible_searchparams = params;
+    search(params);
+  });
+
+  BUTTON_PAGEXRIGHT.addEventListener("click", () => {
+
+  });
+
 /*
     const items = [
       { img: "https://via.placeholder.com/100", text: "Item One" }
